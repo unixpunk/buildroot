5f23687a0f0a25acb7fad4f099c4279d458e4042
unixpunk <steve@hackode.com>
2019-01-07 21:39:44 -0600

    Fixed update build script

--- board/pluto/buildgz.sh
+++ board/pluto/buildgz.sh
@@ -1,7 +1,1 @@
-#/bin/sh
-
-cd /home/steve/Desktop/SDR/Pluto/PlutoWeb/plutoweb/overlay
-rm -f ../update.tgz*
-tar -czvf ../update.tgz *
-cd ..
-md5sum -b update.tgz >update.tgz.md5sum
+../../../../PlutoWeb/plutoweb/buildgz.sh
